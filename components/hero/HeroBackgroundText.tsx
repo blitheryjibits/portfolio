1f3814d7fe67edab4d479aa09ceef344640e3ff6
robert thornton <robert.thornton92@gmail.com>
2026-04-20 10:12:44 +0300

    ui: adjusted height and spacing of text elements

--- components/hero/HeroBackgroundText.tsx
+++ components/hero/HeroBackgroundText.tsx
@@ -1,6 +1,6 @@
 export function HeroBackgroundText() {
   return (
-    <div className="absolute -top-10 right-0 opacity-10 pointer-events-none select-none">
+    <div className="absolute -top-8 right-0 opacity-10 pointer-events-none select-none">
       <h1 className="text-6xl md:text-9xl lg:text-[12rem] font-black text-[#bc13fe] font-headline tracking-tighter leading-none uppercase">
         ROBERT
       </h1>
--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -4,7 +4,7 @@ import { HeroBackgroundText } from "./HeroBackgroundText";
 export function HeroSection() {
   return (
     <section
-      className="relative flex flex-col items-center justify-center min-h-179"
+      className="relative flex flex-col items-center justify-center pt-8 md:pt-24 lg:pt-32"
       id="hero"
     >
       <TerminalWindow />
